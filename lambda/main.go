ea155e0bd0b20f975a84c4e0fa57814f7051bfbe
Saba-Var <vartasashvili94@gmail.com>
2025-01-07 21:23:12 +0400

    add AWS Lambda function implementation and module files

---
+++ lambda/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	Username string `json:"username"`
+}
+
+func HandleRequest(event Event) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username is required")
+	}
+
+	return fmt.Sprintf("Successfully called lambda function with username: %s", event.Username), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
