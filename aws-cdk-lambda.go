97925b9c5fb8b78da9eceb1512cd0216f5e87107
Saba-Var <vartasashvili94@gmail.com>
2025-01-11 18:49:48 +0400

    add API Gateway integration for user registration and login

--- aws-cdk-lambda.go
+++ aws-cdk-lambda.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -35,6 +36,25 @@ func NewAwsCdkLambdaStack(scope constructs.Construct, id string, props *AwsCdkLa
 	})
 
 	table.GrantReadWriteData(lambdaFunc)
+
+	api := awsapigateway.NewRestApi(stack, jsii.String("awsAPIGateway"), &awsapigateway.RestApiProps{
+		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+			AllowHeaders: jsii.Strings("Content-type", "Authorization"),
+			AllowMethods: jsii.Strings("OPTIONS", "GET", "POST", "PUT", "DELETE"),
+			AllowOrigins: jsii.Strings("*"),
+		},
+		DeployOptions: &awsapigateway.StageOptions{
+			LoggingLevel: awsapigateway.MethodLoggingLevel_INFO,
+		},
+	})
+
+	integration := awsapigateway.NewLambdaIntegration(lambdaFunc, nil)
+
+	registerResource := api.Root().AddResource(jsii.String("register"), nil)
+	registerResource.AddMethod(jsii.String("POST"), integration, nil)
+
+	loginResource := api.Root().AddResource(jsii.String("login"), nil)
+	loginResource.AddMethod(jsii.String("POST"), integration, nil)
 
 	return stack
 }
--- lambda/types/types.go
+++ lambda/types/types.go
@@ -1,6 +1,34 @@
 package types
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type RegisterUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type User struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"password"`
+}
+
+func (u *User) NewUser() (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.PasswordHash), bcrypt.DefaultCost)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Username:     u.Username,
+		PasswordHash: string(hashedPassword),
+	}, nil
+}
+
+func ComparePasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
 }
